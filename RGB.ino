08086bee676ebc92a95198aa7e845ab219d3b467
LonerTop <61340353+oppotop9@users.noreply.github.com>
2020-07-07 21:00:27 +0700

    Create RGB.ino

---
+++ RGB.ino
@@ -0,0 +1,43 @@
+int R = 9; int G = 10; int B =11; 
+
+
+
+void setup() {
+  tone(8,500,250);
+  delay(500);
+  
+
+  pinMode(R, OUTPUT);
+
+  pinMode(B, OUTPUT);
+
+  pinMode(G, OUTPUT);
+  
+  Serial.begin(9600);
+}
+
+void loop(){
+    analogWrite(R, 255); // RED 
+    analogWrite(B, 0); 
+    analogWrite(G, 0); 
+
+    delay(500); 
+
+    analogWrite(R, 0); // green
+    analogWrite(B, 0); 
+    analogWrite(G, 255); 
+
+    delay(500);
+
+    analogWrite(R, 255); // yellow
+    analogWrite(B, 0); 
+    analogWrite(G, 255); 
+
+    delay(500);
+
+    analogWrite(R, 0); // blue
+    analogWrite(B, 255); 
+    analogWrite(G, 0); 
+    
+    delay(500);
+}
